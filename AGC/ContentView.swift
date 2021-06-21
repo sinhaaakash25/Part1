788d5263599f2b41a781499cbcbf1e0c6aa7c17b
aakash sinha <aksinha2509@gmail.com>
2021-06-21 16:48:56 +0530

    Initial Commit

---
+++ AGC/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AGC
+//
+//  Created by Aakash Sinha on 21/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
